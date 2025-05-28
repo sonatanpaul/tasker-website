ef283bbdaef3dc4248ee43c500212b0f895d5a7b
sonatanpaul <iamsonatanpaul@gmail.com>
2025-05-28 18:06:07 +0600

    hero section and footer task implement is complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import HeroSection from "./components/hero-section/HeroSection";
 
 function App() {
   return (
     <>
       <Header />
+      <HeroSection />
+      <Footer />
     </>
   );
 }

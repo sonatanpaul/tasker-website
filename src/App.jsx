2a1c5d32d2b30682c4f3f923694e270f6dd3ae90
sonatanpaul <iamsonatanpaul@gmail.com>
2025-05-28 18:31:18 +0600

    task borad implement is complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import HeroSection from "./components/hero-section/HeroSection";
+import TaskBoard from "./components/task-board/TaskBoard";
 
 function App() {
   return (
     <>
       <Header />
       <HeroSection />
+      <TaskBoard />
       <Footer />
     </>
   );
--- src/components/hero-section/HeroSection.jsx
+++ src/components/hero-section/HeroSection.jsx
@@ -3,7 +3,7 @@ import HeroBannerImage from "../../assets/frame.png";
 export default function HeroSection() {
   return (
     <>
-      <section className="pb-[114px] pt-20 md:mt-[100px]">
+      <section className="pb-[114px] flex justify-center pt-20 md:mt-[100px]">
         <div className="container lg:px-20">
           <div className="grid items-center gap-6 md:grid-cols-2">
             <div className="flex justify-center md:order-2">
